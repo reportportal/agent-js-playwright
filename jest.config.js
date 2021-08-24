64147f56f1cad51a2a5275a5147cea49b3354591
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2021-08-24 21:26:41 +0300

    Add ts based project structure

---
+++ jest.config.js
@@ -0,0 +1,41 @@
+/*
+ *  Copyright 2021 EPAM Systems
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: { '.(ts|tsx)': 'ts-jest' },
+  testEnvironment: 'node',
+  testRegex: '/__tests__/.*\\.(test|spec)?\\.(ts|js)$',
+  moduleFileExtensions: ['ts', 'js'],
+  collectCoverageFrom: ['src/**/*.ts'],
+  coveragePathIgnorePatterns: ['/__tests__/', 'index.ts', 'src/types'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        pathRegex: '(/__tests__/.*?\\.(test|spec))\\.(ts|js)$',
+      },
+    },
+  },
+};
