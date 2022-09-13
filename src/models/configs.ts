dd910b31839a7292ad0b4e7558f9d8c2b2663932
Ilya <ilya_hancharyk@epam.com>
2022-09-13 20:31:00 +0300

    EPMRPP-79577 || Update readme & changelog. Prepare for release (#63)

--- src/models/configs.ts
+++ src/models/configs.ts
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2021 EPAM Systems
+ *  Copyright 2022 EPAM Systems
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ export interface ReportPortalConfig {
   rerun?: boolean;
   rerunOf?: string;
   mode?: LAUNCH_MODES;
-  isLaunchMergeRequired?: boolean;
+  isLaunchMergeRequired?: boolean; // not used for this agent
   skippedIssue?: boolean;
   includeTestSteps?: boolean;
   restClientConfig?: RestClientConfig;
