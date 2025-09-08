39b24e7fdb49205a6e8b7206fd7f34f7b786939a
Ilya_Hancharyk <Ilya_Hancharyk@epam.com>
2025-09-08 19:00:19 +0200

    Fix linter errors

--- src/__tests__/reportingApi.spec.ts
+++ src/__tests__/reportingApi.spec.ts
@@ -73,7 +73,7 @@ jest.mock('@playwright/test', () => ({
 
 describe('reportingApi', () => {
   jest.spyOn(helpers, 'now').mockReturnValue(mockedDate);
-  
+
   test('addAttributes should call sendEventToReporter with params', () => {
     const attrs = [
       {
