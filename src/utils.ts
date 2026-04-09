a2a53947dbd29aab7b4ba4a37d62be6e3878fafb
maria-hambardzumian <maria_hambardzumyan@gmail.com>
2026-04-09 12:26:21 +0400

    EPMRPP-113919 || Track test framework version

--- src/utils.ts
+++ src/utils.ts
@@ -19,7 +19,11 @@ import { TestCase, TestResult, TestStatus } from '@playwright/test/reporter';
 import fs from 'fs';
 import path from 'path';
 // @ts-ignore
-import { name as pjsonName, version as pjsonVersion } from '../package.json';
+import {
+  name as pjsonName,
+  version as pjsonVersion,
+  devDependencies as pjsonDevDeps,
+} from '../package.json';
 import { Attachment, AttachmentsConfig, Attribute } from './models';
 import {
   BASIC_ATTACHMENT_CONTENT_TYPES,
@@ -34,6 +38,10 @@ import { test } from '@playwright/test';
 
 const fsPromises = fs.promises;
 
+const declaredVersion = ((pjsonDevDeps || {})['@playwright/test'] || '').replace(/^\D+/, '');
+// eslint-disable-next-line @typescript-eslint/no-var-requires,global-require
+const framework_version = require('@playwright/test/package.json').version || declaredVersion;
+
 export const isFalse = (value: string | boolean | undefined): boolean =>
   [false, 'false'].includes(value);
 
@@ -42,9 +50,10 @@ export const promiseErrorHandler = (promise: Promise<void>, message = ''): Promi
     console.error(message, err);
   });
 
-export const getAgentInfo = (): { version: string; name: string } => ({
+export const getAgentInfo = (): { version: string; name: string; framework_version?: string } => ({
   version: pjsonVersion,
   name: pjsonName,
+  framework_version,
 });
 
 export const getSystemAttribute = (): Attribute => ({
